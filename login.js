443312b64d7203d49b082fac4b463bfeb856ff79
whawko <bra.banda@duocuc.cl>
2026-09-05 18:26:40 -0400

    AGREGADOS
    habia hecho mal un commit y este es el correcto

--- login.js
+++ login.js
@@ -2,7 +2,6 @@ function ingresar() {
     let correo = document.getElementById("Correo").value;
     let clave = document.getElementById("Clave").value;
 
-
     if (correo === "" || clave === "") {
         alert("Por favor, completa todos los campos para ingresar.");
         return;
@@ -14,17 +13,15 @@ function ingresar() {
         return;
     }
 
-
+    // REDIRECCIÓN A vistaAdmin.html
     if (correo === "admin@sanmarcos.cl" && clave === "admin123") {
         alert("Bienvenido al panel de administración de San Marcos.");
-        window.location.href = "admin-vet.html";
+        window.location.href = "vistaAdmin.html"; 
     }
-
     else if (correo === "cliente@email.com" && clave === "1234") {
         alert("Bienvenido. Redirigiendo al historial de tu mascota.");
         window.location.href = "cliente-vet.html";
     }
-
     else {
         alert("Correo o contraseña incorrectos. Intenta nuevamente.");
     }
